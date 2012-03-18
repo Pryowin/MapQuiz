3abf92451bfe9afa60257aecdb5bae2ee06ec426
David Burke <dburke@David-Mac.local>
2012-03-17 17:01:18 -0700

    Map displaying correctly

--- MapDisplay/AFNViewController.h
+++ MapDisplay/AFNViewController.h
@@ -10,4 +10,6 @@
 
 @interface AFNViewController : UIViewController
 
+@property(nonatomic, retain) IBOutlet UIWebView *webView;
+
 @end
--- MapDisplay/AFNViewController.m
+++ MapDisplay/AFNViewController.m
@@ -14,10 +14,25 @@
 
 @implementation AFNViewController
 
+@synthesize webView;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+   
+    //Get path of HTML, get base URL, load page
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"map" ofType:@"svg"];
+    NSData *svgData = [NSData dataWithContentsOfFile:path];
+    NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+    NSURL *baseURL = [[NSURL alloc] initFileURLWithPath:resourcePath isDirectory:YES];
+    
+    [self.webView 	loadData:svgData 
+                   MIMEType:@"image/svg+xml"	
+           textEncodingName:@"UTF-8" 
+                    baseURL:baseURL];
+    
 }
 
 - (void)viewDidUnload
